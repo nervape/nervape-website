edcb4a243c98b2f4d05bef662d46b37b2a8bcbd5
liuminquan <liuminquan.cn@gmail.com>
2022-03-02 11:36:37 +0800

    config replace

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,16 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: "./",
-  plugins: [react()],
-  server: {
-    host: "0.0.0.0",
-  },
+export default defineConfig(({ command, mode }) => {
+  let base = "./";
+  if (command === "serve") {
+  }
+  return {
+    base: base,
+    // base: "/",
+    plugins: [react()],
+    server: {
+      host: "0.0.0.0",
+    },
+  };
 });
