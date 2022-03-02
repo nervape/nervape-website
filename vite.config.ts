9bebb448611d617abb3ffead3497324d560b7ae0
liuminquan <liuminquan.cn@gmail.com>
2022-03-02 18:28:06 +0800

    reset base url

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode }) => {
-  let base = "./";
+  let base = "/";
   if (command === "serve") {
   }
   return {
