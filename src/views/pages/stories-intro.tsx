63e0d991372d8c420e477ccb3584cd11d4222be9
李昭 <leaf.li@showbrain.cn>
2022-06-02 13:20:27 +0800

    调整故事内容间距及banner点击热区

--- src/views/pages/stories-intro.tsx
+++ src/views/pages/stories-intro.tsx
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import "./stories-intro.less";
 import { Story } from "../../nervape/story";
 import { StoryCard } from "../components/story-card";
+import { NavTool } from "../../route/navi-tool";
 
 export class StoriesIntro extends Component<{
   latest?: Story;
@@ -10,7 +11,19 @@ export class StoriesIntro extends Component<{
     const { latest } = this.props;
     console.log("StoriesIntro", latest);
     return (
-      <div className="stories-intro">
+      <div 
+        className="stories-intro"
+        onClick={(e) => {
+          e.preventDefault();
+          e.stopPropagation();
+          NavTool.fnJumpToPage(
+            `/story/${latest?.id}`
+          );
+          window.scrollTo({
+            top: 0,
+            behavior: "smooth",
+          });
+        }}>
         <img
           className="src-img"
           src={latest?.bannerUrl}
