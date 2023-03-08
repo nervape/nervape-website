529bd2165a818390f497546ff31b375e5ea598d7
李昭 <leaf.li@showbrain.cn>
2023-03-08 10:22:38 +0800

    merge 2d_pfp to develop

--- src/views/nervape-composite/index.tsx
+++ src/views/nervape-composite/index.tsx
@@ -1,21 +1,45 @@
-import React, { useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import './index.less';
 
 import LandingBanner from '../../assets/landing-page/banner.png';
 import DownArrow from '../../assets/landing-page/arrow_down.svg';
-import UpArrowIcon from '../../assets/landing-page/up_arrow.svg';
+import DownArrowIcon from '../../assets/nacp/down_arrow.svg';
+import NacpTitle from '../../assets/nacp/nacp.svg';
+import NacpMTitle from '../../assets/nacp/nacp_m.svg';
+import TwitterIcon from '../../assets/nacp/twitter.svg';
+import DiscodeIcon from '../../assets/nacp/discode.svg';
+import FreeMint from '../../assets/nacp/free_mint.png';
 import { Question } from "../../nervape/composite";
 import { PfpMocks } from "../../mock/composite-mock";
 import Footer from "../components/footer";
 import { nervapeApi } from "../../api/nervape-api";
+import { DataContext } from "../../utils/utils";
+import { Tooltip } from "antd";
+
+export type Parthership = {
+    tag: string;
+    date: string;
+    title: string;
+    desc: string;
+}
+
+export type Intro = {
+    cover: string;
+    title: string;
+    desc: string;
+}
 
 export default function Composite() {
+    const { state } = useContext(DataContext);
     const [showLandingPage, setShowLandingPage] = useState(false);
-    const [showBoneList, setShowBoneList] = useState(true);
+    const [showBoneList, setShowBoneList] = useState(false);
     const [godwokenAddress, setGodwokenAddress] = useState('');
+    const [isBonelist, setIsBonelist] = useState(false);
+    const [open, setOpen] = useState(false);
 
     const [clickPosition, setClickPosition] = useState({ x: 0, y: 0 });
-    const [introItems, setIntroItems] = useState<{ title: String, desc: String }[]>([]);
+    const [introItems, setIntroItems] = useState<Intro[]>([]);
+    const [parthershipItems, setParthershipItems] = useState<Parthership[]>([]);
 
     const [questions, setQuestions] = useState<Question[]>([]);
 
@@ -23,9 +47,23 @@ export default function Composite() {
         const { item } = props;
         return (
             <div className="intro-item">
-                <img className="intro-img" alt="" />
-                <div className="intro-title">Assemble Your Own PFP</div>
-                <div className="intro-desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</div>
+                <img className="intro-img" src={item.cover} alt="" />
+                <div className="intro-title">{item.title}</div>
+                <div className="intro-desc">{item.desc}</div>
+            </div>
+        );
+    }
+
+    const ParthershipItem = (props: { item: any; }) => {
+        const { item } = props;
+        return (
+            <div className="parthership-item">
+                <div className="parthership-tag">
+                    <div className="tag">{item.tag}</div>
+                    <div className="date">{item.date}</div>
+                </div>
+                <div className="parthership-title">{item.title}</div>
+                <div className="parthership-desc">{item.desc}</div>
             </div>
         );
     }
@@ -34,16 +72,39 @@ export default function Composite() {
         setShowLandingPage(true);
         setIntroItems([
             {
-                title: '',
-                desc: ''
+                cover: FreeMint,
+                title: 'Free Mint',
+                desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
             },
             {
-                title: '',
-                desc: ''
+                cover: FreeMint,
+                title: 'Assemble Your Own PFP',
+                desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
             },
             {
-                title: '',
-                desc: ''
+                cover: FreeMint,
+                title: 'Assemble Your Own PFP',
+                desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+            }
+        ]);
+        setParthershipItems([
+            {
+                tag: 'NEW PARTNERSHIP',
+                date: '02/27/2023',
+                title: 'Latest Update Title',
+                desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+            },
+            {
+                tag: 'NEW PARTNERSHIP',
+                date: '02/27/2023',
+                title: 'Latest Update Title',
+                desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+            },
+            {
+                tag: 'NEW PARTNERSHIP',
+                date: '02/27/2023',
+                title: 'Latest Update Title',
+                desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
             }
         ]);
         const _questions = PfpMocks.fnGetQuestions();
@@ -67,37 +128,118 @@ export default function Composite() {
                     <section className="composite-section">
                         <div className="nervape-composite">
                             <div className="composite">
-                                <div className="title">NERVAPE COMPOSITE</div>
-                            </div>
-                            {showBoneList && (
-                                <div className="bone-list">
-                                    <div className="title">Bonelist Lookup</div>
-                                    <div className="address-input">
-                                        <input type="text" value={godwokenAddress} onInput={(e: any) => {
-                                            setGodwokenAddress(e.target.value)
-                                        }} placeholder="Godwoken address" />
-                                        <button 
-                                            className="check-btn cursor" 
-                                            onClick={async () => {
-                                                if (!godwokenAddress) return;
-                                                const res = await nervapeApi.fnSearchBonelist(godwokenAddress);
-                                                console.log(res);
-                                                if (res.data.data >= 2) {}
-                                            }}>CHECK</button>
-                                    </div>
-                                    <div className="tip">
-                                        To be notified as soon as we go live... Join Our <a className="cursor" href="https://discord.com/invite/7br6nvuNHP" target="_blank" rel="noopener noreferrer">Discord</a>
-                                    </div>
+                                <div className="title transition">
+                                    <img src={state.windowWidth === 375 ? NacpMTitle : NacpTitle} />
                                 </div>
-                            )}
+                                <div className="description">
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                                    nisi ut aliquip ex ea commodo consequat.
+                                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
+                                    dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                                    sunt in culpa qui officia deserunt mollit anim id est laborum.
+                                </div>
+                            </div>
                         </div>
                         <div className="intro-items">
                             {introItems.map((item, index) => {
-                                return <IntroItem key={index} item={item}></IntroItem>;
+                                return (
+                                    <div className="intro-content" key={index}>
+                                        <IntroItem item={item}></IntroItem>
+                                        {index == 0 && (
+                                            <div className="bone-list">
+                                                <div className="title">Bonelist Lookup</div>
+                                                <div className="address-input">
+                                                    <Tooltip
+                                                        title={() => {
+                                                            return (
+                                                                <p>{isBonelist ? 'In Bonelist' : 'Not In Bonelist'}</p>
+                                                            );
+                                                        }}
+                                                        placement="bottom"
+                                                        overlayClassName="bonelist-tooltip"
+                                                        color="#506077"
+                                                        open={open}>
+                                                        <input type="text" value={godwokenAddress} onInput={(e: any) => {
+                                                            setGodwokenAddress(e.target.value)
+                                                            setOpen(false);
+                                                        }} placeholder="Ethereum address" />
+                                                    </Tooltip>
+                                                    <button
+                                                        className="check-btn cursor"
+                                                        onClick={async () => {
+                                                            if (!godwokenAddress) return;
+                                                            const res = await nervapeApi.fnSearchBonelist(godwokenAddress);
+                                                            console.log(res);
+                                                            setOpen(true);
+                                                            setIsBonelist(res > 0);
+                                                        }}>CHECK</button>
+                                                </div>
+                                                <div className="tip">
+                                                    To be notified as soon as we go live... Join Our <a className="cursor" href="https://discord.com/invite/7br6nvuNHP" target="_blank" rel="noopener noreferrer">Discord</a>
+                                                </div>
+                                            </div>
+                                        )}
+                                    </div>
+                                );
                             })}
                         </div>
                     </section>
-                    <section className="qa-section">
+
+                    <section className="what-new-section">
+                        <div className="what-new-content">
+                            <div className="title">WHAT’S NEW?</div>
+                            <div className="new-parthership">
+                                {state.windowWidth > 750 && (
+                                    <div className="new-left">
+                                        <div className="cover-image"></div>
+                                        <ParthershipItem item={parthershipItems[0]}></ParthershipItem>
+                                    </div>
+                                )}
+
+                                <div className="new-right">
+                                    <div className="parthership-items">
+                                        {parthershipItems.map((item, index) => {
+                                            return (
+                                                <ParthershipItem key={index} item={item}></ParthershipItem>
+                                            );
+                                        })}
+                                    </div>
+                                    <div className="view-all">
+                                        <div className="view-btn cursor">VIEW ALL</div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </section>
+                    <section className="partner-program-section">
+                        <div className="partner-content">
+                            <div className="partner-top flex-align">
+                                <div className="top-left">
+                                    <div className="title">PARTNER PROGRAM</div>
+                                    <div className="desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </div>
+                                    <div className="join-us cursor">JOIN US</div>
+                                </div>
+                                <div className="top-right"></div>
+                            </div>
+                            <div className="partner-artists">
+                                <div className="artist-title">PARTNER ARTISTS</div>
+                                <div className="artist-imgs flex-center">
+                                    {[1, 2, 3, 4].map(m => {
+                                        return <img key={m} className="artist-img" alt="" />
+                                    })}
+                                </div>
+                            </div>
+                        </div>
+                    </section>
+                    <section className="qa-section flex-justify">
+                        {state.windowWidth > 750 && (
+                            <div className="qa-image-c">
+                                <div className="qa-image"></div>
+                            </div>
+                        )}
+
                         <div className="qa-content">
                             <h3 className="title">Q&A</h3>
                             <div className="questions">
@@ -107,7 +249,6 @@ export default function Composite() {
                                             <div
                                                 key={index}
                                                 className={`question cursor ${question.open && 'open'}`}
-                                                style={{ background: question.background }}
                                                 onMouseDown={(e) => {
                                                     console.log('down', e)
                                                     setClickPosition({ x: e.clientX, y: e.clientY })
@@ -128,26 +269,49 @@ export default function Composite() {
                                                     setClickPosition({ x: 0, y: 0 })
                                                 }}
                                             >
-                                                <div className="hover-cover"></div>
                                                 <div
                                                     className="arrow"
                                                 >
-                                                    <img loading="lazy" src={UpArrowIcon} alt="UpArrowIcon" />
+                                                    <img loading="lazy" src={DownArrowIcon} alt="DownArrowIcon" />
                                                 </div>
                                                 <div className="q">
                                                     <div className="q-l">Q:</div>
                                                     <div className="q-r">{question.question}</div>
                                                 </div>
-                                                {question.open ? (
-                                                    <div className="a">
-                                                        <div className="a-l">A:</div>
-                                                        <div className="a-r" dangerouslySetInnerHTML={{ __html: question.answer }}></div>
+
+                                                <div className={`a ${question.open && 'show'}`}>
+                                                    <div className="a-l">A:</div>
+                                                    <div className="a-r">
+                                                        {question.answer}
                                                     </div>
-                                                ) : ''}
+                                                </div>
                                             </div>
                                         );
                                     })
                                 ) : ''}
+                            </div>
+                        </div>
+                    </section>
+
+                    <section className="join-section">
+                        <div className="join-content">
+                            <div className="title">JOIN OUR COMMUNITY</div>
+                            <div className="desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </div>
+                            <div className="join-icons">
+                                <img
+                                    className="cursor"
+                                    onClick={() => {
+                                        window.open('https://twitter.com/Nervapes')
+                                    }}
+                                    src={TwitterIcon}
+                                    alt="TwitterIcon" />
+                                <img
+                                    className="cursor"
+                                    onClick={() => {
+                                        window.open('https://discord.com/invite/7br6nvuNHP')
+                                    }}
+                                    src={DiscodeIcon}
+                                    alt="DiscodeIcon" />
                             </div>
                         </div>
                         <Footer></Footer>
