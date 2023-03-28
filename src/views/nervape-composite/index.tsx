1ea68ff579658cf5c5379b98851949a6c931aa8a
李昭 <leaf.li@showbrain.cn>
2023-03-28 16:11:34 +0800

    update page scroll anim

--- src/views/nervape-composite/index.tsx
+++ src/views/nervape-composite/index.tsx
@@ -25,6 +25,15 @@ import { Swiper, SwiperSlide, useSwiper, useSwiperSlide } from 'swiper/react';
 import { EffectFade } from 'swiper';
 import "swiper/css";
 
+class OverPackCustomize extends OverPack {
+    always?: boolean;
+
+    constructor(props: any, always: boolean) {
+        super(props);
+        this.always = always;
+    }
+}
+
 export function SwiperPrevButton(props: { index: number }) {
     const { index } = props;
     const swiper = useSwiper();
@@ -168,7 +177,7 @@ export default function Composite() {
                         </section>
 
                         <section className="composite-section">
-                            <OverPack always={false} playScale={0.3} style={{ overflow: 'hidden' }}>
+                            <OverPackCustomize always={false} playScale={0.3} style={{ overflow: 'hidden' }}>
                                 <TweenOne className="nervape-composite"
                                     key="0" animation={{ opacity: 1, delay: 200, duration: 600 }} style={{ opacity: 0 }}>
                                     <div className="composite">
@@ -232,7 +241,7 @@ export default function Composite() {
                                         })}
                                     </div>
                                 </div>
-                            </OverPack>
+                            </OverPackCustomize>
                         </section>
 
                         <section className="minting-phases-section">
