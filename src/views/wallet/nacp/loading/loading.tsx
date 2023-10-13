bbae2decd25b399372d4046fa474166bcb6cfe26
李昭 <leaf.li@showbrain.cn>
2023-10-13 11:31:05 +0800

    add loading

--- src/views/wallet/nacp/loading/loading.tsx
+++ src/views/wallet/nacp/loading/loading.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
-import LoadingGif from '../../../../assets/images/loading.gif';
+import './loading.less';
+
+import LoadingGif from '../../../../assets/wallet/nacp/loading_assets.gif';
 
 export default function LoadingAssetsModal(props: { show: boolean, progress: string }) {
     const { show, progress } = props;
+
     return (
-        <div className={`loading-modal popup-container ${show && 'show'}`}>
+        <div className={`loading-assets-modal popup-container ${show && 'show'}`}>
             <div className="loading-content">
                 <img src={LoadingGif} alt="LoadingGif" />
                 <div className="text">{`Loading assets... ${progress}%`}</div>
