41db069b4f07612c2166ab77258219fd3ec5c547
李昭 <leaf.li@showbrain.cn>
2023-12-14 11:17:04 +0800

    文案修改

--- src/views/nervape-composite/point-map/end/index.tsx
+++ src/views/nervape-composite/point-map/end/index.tsx
@@ -20,7 +20,7 @@ export default function PointMapEnd() {
                     November 2023, we halved ape blast! Nervos’ CKB was halving and we celebrated the four year milestone with our own Halve Ape Blast event (links below)!
                     <br />
                     <br />
-                    We Nervos’d up our apes with new design assets so that everyone could make their own *Halve Nervapes.* They then claimed a block on our collaborative Halve Ape Blast Canvas to place their ape. 🦍 Every participant that claimed a block was airdropped an NFT of the full canvas (above) to their Joy ID account (<a href="https://joy.id/" target="_blank">https://joy.id/</a> ). 🖼️
+                    We Nervos’d up our apes with new design assets so that everyone could make their own Halve Nervapes. They then claimed a block on our collaborative Halve Ape Blast Canvas to place their ape. 🦍 Every participant that claimed a block was airdropped an NFT of the full canvas (above) to their <a href="https://joy.id/" target="_blank">Joy ID</a> account. 🖼️
                     <br />
                     <br />
                     Below we have a timelapse video of the canvas from start to finish. Watch a week of creating compressed into less than 43 seconds. What shapes do you see forming on our canvas? 🤔
